77458408645b1ed56d097883735fe650327a9cd6
Haruka0522 <harukapc1@gmail.com>
2020-07-18 10:43:26 +0900

    add sample 3D model

--- control_gui/control_gui.py
+++ control_gui/control_gui.py
@@ -1,8 +1,20 @@
 import sys
 
 from PyQt5.QtCore import Qt, QTimer
-from PyQt5.QtWidgets import QWidget, QApplication, QSlider, QGridLayout, QVBoxLayout, QHBoxLayout, QLabel
+from PyQt5.QtWidgets import QWidget, QApplication, QSlider, QGridLayout, QVBoxLayout, QHBoxLayout, QLabel, QFrame, QMainWindow,QDesktopWidget
 from PyQt5.QtGui import QImage, QPixmap
+
+import vtk
+from vtk.util.colors import tomato
+from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
+
+
+class MouseInteractorStyle(vtk.vtkInteractorStyleTrackballCamera):
+    def __init__(self, parent=None):
+        self.AddObserver("LeftButtonPressEvent", self.leftButtonPressEvent)
+
+    def leftButtonPressEvent(self, obj, event):
+        self.OnLeftButtonDown()
 
 
 class MyWindow(QWidget):
@@ -20,6 +32,7 @@ class MyWindow(QWidget):
         self.create_how_to_image()
         self.create_sliders()
         self.create_flipper_image()
+        self.create_3Dmodel()
 
         self.setWindowTitle(self.title)
         self.setGeometry(0, 0, self.width, self.height)
@@ -34,7 +47,7 @@ class MyWindow(QWidget):
         imageLabel.setPixmap(QPixmap.fromImage(image))
         imageLabel.scaleFactor = 1.0
         image_layout.addWidget(imageLabel)
-        self.main_layout.addLayout(image_layout, 0, 0)
+        self.main_layout.addLayout(image_layout,1,0)
 
     def create_how_to_image(self):
         image_layout = QHBoxLayout()
@@ -43,7 +56,7 @@ class MyWindow(QWidget):
         imageLabel.setPixmap(QPixmap.fromImage(image))
         imageLabel.scaleFactor = 1.0
         image_layout.addWidget(imageLabel)
-        self.main_layout.addLayout(image_layout, 0, 1)
+        self.main_layout.addLayout(image_layout,0,1)
 
     def create_sliders(self):
         self.sliders_layout = QHBoxLayout()
@@ -64,7 +77,51 @@ class MyWindow(QWidget):
         self.slider_BR.setMaximum(180)
         self.sliders_layout.addWidget(self.slider_BR)
 
-        self.main_layout.addLayout(self.sliders_layout, 1, 0)
+        self.main_layout.addLayout(self.sliders_layout,1,1)
+
+    def create_3Dmodel(self):
+        # source
+        cylinder = vtk.vtkCylinderSource()
+        cylinder.SetResolution(20)
+
+        # mapper
+        mapper = vtk.vtkPolyDataMapper()
+        mapper.SetInputConnection(cylinder.GetOutputPort())
+
+        # actor
+        actor = vtk.vtkActor()
+        actor.SetMapper(mapper)
+        actor.GetProperty().SetColor(tomato)
+        actor.RotateX(30.)
+        actor.RotateY(-45.)
+
+        # renderer
+        ren = vtk.vtkRenderer()
+        ren.AddActor(actor)
+        ren.SetBackground(0.1, 0.2, 0.4)
+
+        # interactor
+        frame = QFrame()
+        inter = QVTKRenderWindowInteractor(frame)
+        inter.SetInteractorStyle(MouseInteractorStyle())
+
+        ren_win = inter.GetRenderWindow()
+        ren_win.AddRenderer(ren)
+
+        ren.ResetCamera()
+        ren.GetActiveCamera().Zoom(1.5)
+
+        ren_win.Render()
+        inter.Initialize()
+
+        model_layout = QVBoxLayout()
+        model_layout.addWidget(frame)
+        self.main_layout.addLayout(model_layout,0,0)
+
+    def centerOnScreen(self):
+        res = QDesktopWidget().screenGeometry()
+        self.move((res.width() / 2) - (self.frameSize().width() / 2),
+                  (res.height() / 2) - (self.frameSize().height() / 2))
 
     def _run(self):
         if self.counter == 360:
